e79772d423deb6ed37ffcb6d2ac428004850ad58
Sean Malan <smseanmalan@gmail.com>
2024-04-09 17:55:39 +1200

    added env variables to keep dbconnection secure

--- athop/settings.py
+++ athop/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +77,23 @@ WSGI_APPLICATION = 'athop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+load_dotenv()
+
+DB_NAME = os.environ.get('DB_NAME'),
+DB_USER = os.environ.get('DB_USER'),
+DB_PASSWORD = os.environ.get('DB_PASSWORD'),
+DB_HOST = os.environ.get('DB_HOST'),
+DB_PORT = os.environ.get('DB_PORT'),
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DB_NAME',
+        'USER': 'DB_USER',
+        'PASSWORD': 'DB_PASSWORD',
+        'HOST': 'DB_HOST',
+        'PORT': 'DB_PORT',
     }
 }
 
