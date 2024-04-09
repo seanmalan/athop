62eb7bf18e5782f69c9e9d5f969c9b61eac6cdea
Sean Malan <smseanmalan@gmail.com>
2024-04-09 18:11:13 +1200

    connection to railway db is successful

--- athop/settings.py
+++ athop/settings.py
@@ -79,21 +79,20 @@ WSGI_APPLICATION = 'athop.wsgi.application'
 
 load_dotenv()
 
-DB_NAME = os.environ.get('DB_NAME'),
-DB_USER = os.environ.get('DB_USER'),
-DB_PASSWORD = os.environ.get('DB_PASSWORD'),
-DB_HOST = os.environ.get('DB_HOST'),
-DB_PORT = os.environ.get('DB_PORT'),
-
+DB_NAME = os.environ.get('DB_NAME')
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DB_NAME',
-        'USER': 'DB_USER',
-        'PASSWORD': 'DB_PASSWORD',
-        'HOST': 'DB_HOST',
-        'PORT': 'DB_PORT',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
     }
 }
 
